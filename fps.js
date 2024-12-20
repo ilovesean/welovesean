07b724463d6ccc969f3f33f8bef5698dd5982573
ilovesean <jhandy5700@inst.hcpss.org>
2024-12-19 22:48:46 -0500

    Update fps.js

--- fps.js
+++ fps.js
@@ -1,1 +1,19 @@
+(function() {
+    let lastFrameTime = performance.now();
+    let frameCount = 0;
+
+    function updateFPS() {
+        const now = performance.now();
+        frameCount++;
+        if (now >= lastFrameTime + 1000) {
+            const fps = frameCount;
+            frameCount = 0;
+            lastFrameTime = now;
+            document.getElementById('fps-counter').textContent = `FPS: ${fps}`;
+        }
+        requestAnimationFrame(updateFPS);
+    }
+
+    requestAnimationFrame(updateFPS);
+})();
 
