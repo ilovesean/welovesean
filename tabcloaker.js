1d431eb0e5a61e8310bee1f741c3165f9a1a2e39
ilovesean <jhandy5700@inst.hcpss.org>
2024-12-19 23:02:21 -0500

    Update tabcloaker.js

--- tabcloaker.js
+++ tabcloaker.js
@@ -1,1 +1,17 @@
+(function() {
+    const originalTitle = document.title;
+    const originalFavicon = document.querySelector("link[rel='icon']");
+    const cloakedTitle = "Study Mode";
+    const cloakedFavicon = "images/study.png"; // Make sure to have a "study.png" image in your images folder
+
+    document.addEventListener('visibilitychange', function() {
+        if (document.hidden) {
+            document.title = cloakedTitle;
+            originalFavicon.setAttribute("href", cloakedFavicon);
+        } else {
+            document.title = originalTitle;
+            originalFavicon.setAttribute("href", "images/favicon.png"); // Ensure your favicon is named correctly
+        }
+    });
+})();
 
