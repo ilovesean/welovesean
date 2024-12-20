3edcb9055470c979342fef5b0181877e8d052030
ilovesean <jhandy5700@inst.hcpss.org>
2024-12-19 22:48:35 -0500

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,1 +1,9 @@
+function toggleTheme() {
+    const themeStyle = document.getElementById('theme-style');
+    if (themeStyle.getAttribute('href') === 'css/lightmode.css') {
+        themeStyle.setAttribute('href', 'css/darkmode.css');
+    } else {
+        themeStyle.setAttribute('href', 'css/lightmode.css');
+    }
+}
 
