7ff94f15c163d4dcc43bd629f3f1dd21343de00f
ilovesean <jhandy5700@inst.hcpss.org>
2024-12-19 22:48:59 -0500

    Update tabcloker.js

--- tabcloker.js
+++ tabcloker.js
@@ -1,1 +1,12 @@
+(function() {
+    let lastTime = Date.now();
+    document.addEventListener('visibilitychange', function() {
+        if (document.hidden) {
+            lastTime = Date.now();
+        } else {
+            const deltaTime = Date.now() - lastTime;
+            alert(`You were away for ${Math.floor(deltaTime / 1000)} seconds!`);
+        }
+    });
+})();
 
